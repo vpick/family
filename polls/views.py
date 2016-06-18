8574e0be18e8eae229aae435ccd0b59c90950897
vpick <varsha123verma@gmail.com>
2016-06-18 17:12:55 +0530

    all files are modified

---
+++ polls/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import loader
+from django.http import Http404 
+from django.views import generic 
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, render
+from .models import Choice, Question
+
+
+def index(request):
+    return HttpResponse("hello,world. you're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("you are looking at question %s" % question_id)
+
+def results(request, question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html',{'question':question})
+
+def vote(request, question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message':"you didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def index(request):
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    template=loader.get_template('polls/index.html')
+    context={'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    #question=Question.objects.get(pk=question_id)
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name='latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.orderby(' -pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name= 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
--- vfamily/settings.py
+++ vfamily/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles','blog',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'vfamily.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dpolls',
+        'USER': 'root',
+        'PASSWORD': 'varsha',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
